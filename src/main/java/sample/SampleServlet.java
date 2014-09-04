5ad09bd95ef907fc22617ac7c9f3b0ec26b8a218
yakisuzu <yakisuzu>
2014-09-05 00:46:06 +0900

    new

---
+++ src/main/java/sample/SampleServlet.java
@@ -0,0 +1,20 @@
+package sample;
+
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import supers.SuperServlet;
+
+@WebServlet("/SampleServlet")
+public class SampleServlet extends SuperServlet {
+	private static final long serialVersionUID = 1L;
+
+	@Override
+	protected void doServlet(HttpServletRequest req, HttpServletResponse res) {
+		SampleBean bean = new SampleBean();
+		bean.setId("iddddddd");
+		bean.setName("nameeee");
+		req.setAttribute("SampleBean", bean);
+	}
+}
