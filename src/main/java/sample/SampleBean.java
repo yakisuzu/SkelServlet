9307ef925073ddb0c65505d81402f839976e69f3
yakisuzu <yakisuzu@gmail.com>
2014-09-05 00:46:06 +0900

    new

---
+++ src/main/java/sample/SampleBean.java
@@ -0,0 +1,27 @@
+package sample;
+
+public class SampleBean {
+	private String id;
+	private String name;
+
+	public SampleBean() {
+		id = "";
+		name = "";
+	}
+
+	public String getId() {
+		return id;
+	}
+
+	public void setId(String id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+}
